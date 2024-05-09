8139cd3cb3523b05de1ca8975cdc5ed09a25fbb7
Osman Ulutaş <osman.ulutas@outlook.com>
2024-05-09 03:51:20 +0300

    SearchService Created

---
+++ src/SearchService/SearchService/Program.cs
@@ -0,0 +1,48 @@
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.SeedData;
+using SearchService.Services;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message);
+}
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
---
+++ src/SearchService/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+    {
+        public readonly HttpClient _httpClient = httpClient;
+        public readonly IConfiguration _config = config;
+
+        public async Task<List<Item>> GEtItemsForSearchDb()
+        {
+            var lastUpdate = await DB.Find<Item, string>()
+                .Sort(x => x.Descending(x => x.UpdatedAt))
+                .Project(x => x.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+            return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "api/auctions?date="+lastUpdate);
+        }
+    }
+}
